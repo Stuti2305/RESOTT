b1f2b67969eb92dca4bc085b1f90fd9433fe5213
Stuti mishra <83034589+Stuti2305@users.noreply.github.com>
2025-09-10 16:15:02 +0530

    Delete doorstep2-main directory

--- doorstep2-main/dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'doorstep',
-  location: 'us-central1'
-};
-
--- doorstep2-main/src/components/Spinner.tsx
+++
@@ -1,6 +0,0 @@
-// components/Spinner.tsx
-export const Spinner = () => {
-  return (
-    <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-[#FF5733]"></div>
-  );
-};
